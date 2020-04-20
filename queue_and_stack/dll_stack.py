400dce8d131f0ad65481a8a3424c7e9dcb7269e4
Greg Johnston <gcjohnston91@gmail.com>
2020-04-20 16:42:19 -0400

    Fighting errors in PyCharm. Not sure why 2/4 tests in stack are failing

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,22 @@
 import sys
+
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
 class Stack:
-    def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+	def __init__(self):
+		self.size = 0
+		# Why is our DLL a good choice to store our elements?
+		self.storage = DoublyLinkedList
 
-    def push(self, value):
-        pass
+	def push(self, value):
+		self.storage.add_to_head(value)
 
-    def pop(self):
-        pass
+	def pop(self):
+		if self.size == 0:
+			return None
+		self.storage.remove_from_head()
 
-    def len(self):
-        pass
+	def len(self):
+		return self.size
